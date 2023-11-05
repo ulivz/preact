6d90cc5e698c202100214f12bdef7a1de79f9e49
chenhaoli <chenhaoli@bytedance.com>
2023-11-05 18:14:59 +0800

    chore(example): enable web doctor

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,6 +1,10 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const isProd = process.env.NODE_ENV === 'production';
+/**
+ * @see https://web-doctor.bytedance.net/guide/quick-start/build.html#webpack-%E9%A1%B9%E7%9B%AE%E6%8E%A5%E5%85%A5
+ */
+const { WebDoctorWebpackPlugin } = require("@web-doctor/webpack-plugin");
 
 /**
  * @type {import('webpack').Configuration}
@@ -23,8 +27,13 @@ module.exports = {
 		new HtmlWebpackPlugin({
 			template: 'src/index.html',
 			inject: 'body'
-		})
-	],
+		}),
+		// process.env.NODE_ENV === "production" && // NODE_ENV === "production" 是因为建议在 webpack 的 mode 为 'production' 时执行插件；
+		process.env.ANALYZE && // 建议在 ANALYZE 为 true 时执行分析插件，因为插件可能会拉长一些构建时间，建议本地或 CI 或 BOE 环境下执行 web doctor 分析能力；
+		  new WebDoctorWebpackPlugin({
+			disableClientServer: Boolean(process.env.CI_ACTOR), // 建议 CI 环境下关闭 server 能力，否则会卡住 pipeline 流程，检测 CI 环境可以使用 `process.env.CI_ACTOR`.
+		  }),
+	  ].filter(Boolean),
 	module: {
 		rules: [
 			{
@@ -68,5 +77,6 @@ module.exports = {
 
 	optimization: {
 		runtimeChunk: 'single'
-	}
+	},
+	
 };
