6d5f504b67e4235a87af21e9045bae7d3cc597a5
chenhaoli <chenhaoli@bytedance.com>
2023-11-05 22:36:00 +0800

    chore(example): eanble source code debugging

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,10 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const isProd = process.env.NODE_ENV === 'production';
-/**
- * @see https://web-doctor.bytedance.net/guide/quick-start/build.html#webpack-%E9%A1%B9%E7%9B%AE%E6%8E%A5%E5%85%A5
- */
-const { WebDoctorWebpackPlugin } = require("@web-doctor/webpack-plugin");
 
 /**
  * @type {import('webpack').Configuration}
@@ -27,6 +23,7 @@ module.exports = {
 			'preact/devtools': path.resolve(__dirname, '../devtools/src/index.js'),
 			'preact/debug': path.resolve(__dirname, '../debug/src/index.js'),
 			'preact/jsx-dev-runtime': path.resolve(__dirname, '../jsx-runtime/src/index.js'),
+			'preact/jsx-runtime': path.resolve(__dirname, '../jsx-runtime/src/index.js'),
 			'preact': path.resolve(__dirname, '../src/index.js'),
 		}
 	},
@@ -36,12 +33,7 @@ module.exports = {
 			template: 'src/index.html',
 			inject: 'body'
 		}),
-		// process.env.NODE_ENV === "production" && // NODE_ENV === "production" 是因为建议在 webpack 的 mode 为 'production' 时执行插件；
-		process.env.ANALYZE && // 建议在 ANALYZE 为 true 时执行分析插件，因为插件可能会拉长一些构建时间，建议本地或 CI 或 BOE 环境下执行 web doctor 分析能力；
-		  new WebDoctorWebpackPlugin({
-			disableClientServer: Boolean(process.env.CI_ACTOR), // 建议 CI 环境下关闭 server 能力，否则会卡住 pipeline 流程，检测 CI 环境可以使用 `process.env.CI_ACTOR`.
-		  }),
-	  ].filter(Boolean),
+	],
 	module: {
 		rules: [
 			{
@@ -60,32 +52,35 @@ module.exports = {
 				type: 'asset'
 			},
 
+			
 			{
 				test: /.jsx?$/,
-				// exclude: /node_modules/,
-				use: {
-				  loader: "swc-loader",
-				  options: {
-					jsc: {
-					  parser: {
-						syntax: "ecmascript",
-						jsx: true,
-					  },
-					  transform: {
-						react: {
-						  runtime: "automatic",
-						  importSource: "preact",
-						},
-					  },
+				exclude: /node_modules/,
+				use: [
+				  {
+					loader: "babel-loader",
+					options: {
+					  // presets: ["@babel/react"],
+					  plugins: [
+						[
+						  "@babel/transform-react-jsx",
+						  {
+							// @see https://babeljs.io/docs/babel-plugin-transform-react-jsx#react-automatic-runtime-1
+							runtime: "automatic",
+							importSource: "preact",
+						  },
+						],
+					  ],
 					},
 				  },
-				},
+				],
 			  },
+		
 		]
 	},
 
 	optimization: {
 		runtimeChunk: 'single'
 	},
-	
+
 };
