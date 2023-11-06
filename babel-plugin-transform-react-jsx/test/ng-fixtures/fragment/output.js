50f218c64b369f4e56cf644c9a64e8335f4e979e
chenhaoli <chenhaoli@bytedance.com>
2023-11-07 00:13:16 +0800

    feat: add fragment test

---
+++ babel-plugin-transform-react-jsx/test/ng-fixtures/fragment/output.js
@@ -0,0 +1,6 @@
+import { jsx as _jsx } from 'react/jsx-runtime';
+import { Fragment as _Fragment } from 'react/jsx-runtime';
+import { jsxs as _jsxs } from 'react/jsx-runtime';
+_jsxs(_Fragment, {
+  children: [_jsx('h1', {}), _jsx(App, {})]
+});
